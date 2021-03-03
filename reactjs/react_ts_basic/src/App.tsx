ab15c62f69e19f12193d73ac08daa6dd2625c1a8
fajarslvn <jarphish@gmail.com>
2021-03-03 21:14:21 +0700

    add files

---
+++ reactjs/react_ts_basic/src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Header from './Header';
+
+function App() {
+  return (
+    <div className="App">
+      <Header buttonText = 'Click Me!' />      
+    </div>
+  );
+}
+
+export default App;
